a2ecc977eaa1f226be55f548c147e91e70cd4ffe
Paul Popat <p.t.popat@googlemail.com>
2019-07-13 13:58:42 +0100

    Made it look nice

--- src/web/library-viewer.tsx
+++ src/web/library-viewer.tsx
@@ -9,16 +9,18 @@ export const LibraryViewer: SFC<{
     <div className="library-view">
       {Array.from(
         p.library.map(a => (
-          <span className="game-thumbnail" key={a.appid}>
-            <span className="image-container">
-              <img
-                src={`https://steamcdn-a.akamaihd.net/steam/apps/${
-                  a.appid
-                }/capsule_184x69.jpg`}
-                onError={e => (e.currentTarget.style.display = "none")}
-              />
+          <span className="game-thumbnail-container" key={a.appid}>
+            <span className="game-thumbnail">
+              <span className="image-container">
+                <img
+                  src={`https://steamcdn-a.akamaihd.net/steam/apps/${
+                    a.appid
+                  }/capsule_184x69.jpg`}
+                  onError={e => (e.currentTarget.style.display = "none")}
+                />
+              </span>
+              <span className="game-name">{a.name}</span>
             </span>
-            <span className="game-name">{a.name}</span>
           </span>
         ))
       )}
